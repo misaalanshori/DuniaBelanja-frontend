636627ca1c5a99bfa80278aeededc4556c8d9fda
M Isa Al Anshori <isabungsu@gmail.com>
2023-08-16 15:28:12 +0700

    Homepage: Product Category Browser
    added tailwind-scrollbar
    some small styling fixes

--- src/homepage/Homepage.jsx
+++ src/homepage/Homepage.jsx
@@ -1,4 +1,5 @@
 import { Link } from "react-router-dom";
+import { useRef, useState, useEffect } from 'react';
 import DBLogo from '/icons/logo.svg';
 import DBLogoWhite from '/icons/logowhite.svg';
 import heroa from '../assets/images/heroa.png';
@@ -13,7 +14,195 @@ import person1 from '../assets/images/person1.png';
 import person2 from '../assets/images/person2.png';
 import person3 from '../assets/images/person3.png';
 
-import { MdArrowOutward, MdStarOutline, MdLocationSearching, MdOutlineThumbUpAlt } from "react-icons/md";
+import product1 from '../assets/images/product1.png';
+import product2 from '../assets/images/product2.png';
+import product3 from '../assets/images/product3.png';
+import product4 from '../assets/images/product4.png';
+import product5 from '../assets/images/product5.png';
+import product6 from '../assets/images/product6.png';
+
+
+import { MdArrowOutward, MdStarOutline, MdLocationSearching, MdOutlineThumbUpAlt, MdChevronLeft, MdChevronRight, MdEast, MdSearch } from "react-icons/md";
+
+function ProductCard({ image, name, rating, sold }) {
+    const formatter = Intl.NumberFormat('en', { notation: 'compact' });
+    return (
+        <div className="flex flex-col w-48 h-72 rounded-xl overflow-hidden bg-gray-200">
+            <img className="object-cover h-56" src={image}></img>
+            <div className="flex flex-col gap-2 p-2 ">
+                <h5 className="w-full font-semibold text-ellipsis line-clamp-2" >{name}</h5>
+                <div className="flex flex-row items-center text-xs"><MdStarOutline size="16px" color="orange"/> {rating} ratings - {formatter.format(sold)}+ sold</div>
+            </div>
+        </div>
+    )
+}
+
+function StoreCategories() {
+    const [selectedCategory, setSelectedCategory] = useState(0);
+    const [searchQuery, setSearchQuery] = useState("");
+    const dataRef = useRef({})
+
+    function prevCategory() {
+        if (selectedCategory > 0) {
+            setSelectedCategory(selectedCategory - 1);
+        }
+    }
+    function nextCategory() {
+        if (selectedCategory < Object.keys(dataRef.current).length - 1) {
+            setSelectedCategory(selectedCategory + 1);
+        }
+    }
+
+    function stringSearchMatch(str, searchStr) {
+        const words = str.split(" ");
+        const searchWords = searchStr.split(" ");
+        return searchWords.every(word => words.some(w => w.toLowerCase().includes(word.toLowerCase())))
+    }
+
+    // dummy data, actual data may be different
+    useEffect(()=>{
+        dataRef.current = {
+            "Clothing": [
+                {
+                    name: "VILOVE Women Hawaii Shirts Soft Cool Floral",
+                    image: product1,
+                    rating: 4.9,
+                    sold: 2900
+                },
+                {
+                    name: "Fantaslook V Neck T Shirts for Women Floral Summer",
+                    image: product2,
+                    rating: 4.9,
+                    sold: 2900
+                },
+                {
+                    name: "BELLZELY Womens Shirts Short Sleeve",
+                    image: product3,
+                    rating: 4.9,
+                    sold: 2900
+                },
+                {
+                    name: "Casual Men Shirt Long Sleeve Solid Color V Neck",
+                    image: product4,
+                    rating: 4.9,
+                    sold: 2900
+                },
+                {
+                    name: "JWD Mens Henley Long Sleeve T-Shirt Cotton ",
+                    image: product5,
+                    rating: 4.9,
+                    sold: 2900
+                },
+                {
+                    name: "Mens T-Shirts Casual Zipper Turn-Down Blouse",
+                    image: product6,
+                    rating: 4.9,
+                    sold: 2900
+                },
+                {
+                    name: "VILOVE Women Hawaii Shirts Soft Cool Floral",
+                    image: product1,
+                    rating: 4.9,
+                    sold: 2900
+                },
+                {
+                    name: "Fantaslook V Neck T Shirts for Women Floral Summer",
+                    image: product2,
+                    rating: 4.9,
+                    sold: 2900
+                },
+                {
+                    name: "BELLZELY Womens Shirts Short Sleeve",
+                    image: product3,
+                    rating: 4.9,
+                    sold: 2900
+                },
+                {
+                    name: "Casual Men Shirt Long Sleeve Solid Color V Neck",
+                    image: product4,
+                    rating: 4.9,
+                    sold: 2900
+                },
+                {
+                    name: "JWD Mens Henley Long Sleeve T-Shirt Cotton ",
+                    image: product5,
+                    rating: 4.9,
+                    sold: 2900
+                },
+                {
+                    name: "Mens T-Shirts Casual Zipper Turn-Down Blouse",
+                    image: product6,
+                    rating: 4.9,
+                    sold: 2900
+                },
+                
+            ],
+            "Electronics": [],
+            "Beauty & Care": [
+                {
+                    name: "Fantaslook V Neck T Shirts for Women Floral Summer",
+                    image: product2,
+                    rating: 4.9,
+                    sold: 2900
+                },
+                {
+                    name: "BELLZELY Womens Shirts Short Sleeve",
+                    image: product3,
+                    rating: 4.9,
+                    sold: 2900
+                },
+            ],
+            "Home & Kitchen": [],
+            "Sports & Outdoors": [
+                {
+                    name: "Casual Men Shirt Long Sleeve Solid Color V Neck",
+                    image: product4,
+                    rating: 4.9,
+                    sold: 2900
+                },
+                {
+                    name: "JWD Mens Henley Long Sleeve T-Shirt Cotton ",
+                    image: product5,
+                    rating: 4.9,
+                    sold: 2900
+                },
+            ],
+            "Toys & Hobbies": [],
+            "Food & Beverage": [],
+            "Medicine": [],
+            "Category-A": [],
+            "Category-B": [],
+            "Category-C": [],
+            "Category-D": [],
+            
+        }
+    },[])
+    return (
+        <div className="w-11/12 lg:w-4/6 h-[54rem] sm:h-[48rem] flex flex-col justify-between items-stretch gap-4" >
+            <div className="flex flex-row items-center gap-2 w-64 h-10 px-3 rounded-xl bg-gray-200 text-dbblue">
+                <MdSearch size="24px"/>
+                <input className="w-full h-full bg-transparent" type="text" placeholder="Search..." value={searchQuery} onChange={e => setSearchQuery(e.target.value)}/>
+            </div>
+            <div className="flex flex-col sm:flex-row w-full h-full items-center" >
+                <div className="flex flex-row sm:flex-col justify-center w-full sm:w-64 gap-2">
+                    
+                    <ul className="flex flex-row sm:flex-col content-evenly items-stretch px-4 max-sm:overflow-x-scroll sm:overflow-y-scroll scrollbar-thin scrollbar-thumb-dbblue scrollbar-track-gray-300 scrollbar-thumb-rounded-xl scrollbar-track-rounded-xl flex-shrink sm:h-[38rem] text-base sm:text-xl text-dbblue text-center sm:text-left">
+                        {Object.keys(dataRef.current).map((e,i) => (<li onClick={()=>{setSelectedCategory(i)}} className={"hover:bg-black hover:bg-opacity-10 rounded-lg cursor-pointer py-3 px-2" + (i == selectedCategory ? " font-semibold" : "")} key={i}>{e}</li>))}
+                    </ul>
+                    <div className="flex flex-col sm:flex-row justify-center items-center gap-2">
+                        <button onClick={prevCategory} className="rounded-full p-0.5 bg-black hover:bg-dbblue"><MdChevronLeft size="32px" color="white"/></button>
+                        <button onClick={nextCategory} className="rounded-full p-0.5 bg-black hover:bg-dbblue"><MdChevronRight size="32px" color="white"/></button>
+                    </div>
+                </div>
+                <div className="w-full h-[40rem]  flex flex-row flex-wrap gap-4 justify-center content-start overflow-y-scroll scrollbar-thin scrollbar-thumb-dbblue scrollbar-track-gray-300 scrollbar-thumb-rounded-xl scrollbar-track-rounded-xl">
+                    {dataRef.current[Object.keys(dataRef.current)[selectedCategory]]?.filter(e=>stringSearchMatch(e.name, searchQuery)).map(e => (<ProductCard name={e.name} image={e.image} rating={e.rating} sold={e.sold} />))}
+                </div>
+            </div>
+            <Link to="/" className="flex flex-row items-center justify-center rounded-full p-2 gap-2 w-44 bg-dbblue text-white" >All Categories <MdEast size="20px" color="white"/></Link>
+            
+        </div>
+    )
+}
 
 export default function Homepage() {
     return (
@@ -31,11 +220,11 @@ export default function Homepage() {
             </div>
             
 
-            <div className="w-full h-72 md:h-96 lg:h-auto flex flex-row justify-center items-center bg-black px-4 pt-4 pb-24">
+            <div className="w-full h-72 md:h-96 lg:h-auto flex flex-row justify-center items-center bg-black px-4 pt-4 pb-24 overflow-hidden">
                 <h1 className="font-semibold text-4xl md:text-8xl lg:text-9xl text-white tracking-wide">BUILDING <br/> DIFFERENT</h1>
                 <div className="flex flex-col justify-center">
                     <div className="flex flex-col items-center transform rotate-90 text-white">
-                        <a className="w-44 lg:w-56 lg:text-xl flex flex-row justify-center items-center py-2 px-4 gap-2 border-none rounded-full bg-dbblue font-semibold h-min hover:bg-opacity-90 cursor-pointer">
+                        <a className="w-44 lg:w-auto lg:text-xl flex flex-row justify-center items-center py-2 px-4 gap-2 border-none rounded-full bg-dbblue font-semibold h-min hover:bg-opacity-90 cursor-pointer">
                             CONTACT US
                             <div className="border-none rounded-full p-2 bg-[#053677] bg-opacity-70 transform scale-x-[-1]">
                             <MdArrowOutward/>
@@ -56,9 +245,9 @@ export default function Homepage() {
                 
             </div>
 
-            <div className="w-full flex flex-col items-center gap-4">
+            <div className="w-full flex flex-col items-center gap-12">
                 <h1 className="text-center text-5xl sm:text-8xl font-semibold">PRODUCT<br/>CATEGORIES</h1>
-                <div className="w-full lg:w-4/6 h-[48rem] border-4"></div>
+                <StoreCategories/>
             </div>
 
             <div>
@@ -109,7 +298,7 @@ export default function Homepage() {
             </div>
 
             <div className="relative">
-                <div className="absolute w-full h-8 -top-8 bg-white rounded-t-full"></div>
+                <div className="absolute w-full h-9 -top-8 bg-white rounded-t-full"></div>
                 <div className="flex flex-col items-center justify-center text-center gap-8 p-16">
                     <h1 className="text-5xl font-semibold">Why Us?</h1>
                     <p className="w-full sm:w-2/3 text-lg sm:text-xl">"Choosing Excellence, Every Time. Discover the 'Why Us' Behind Our Unparalleled Quality. From Handpicked Selections to Rigorous Testing, We're Committed to Delivering Products That Exceed Expectations. Elevate Your Standards with Dunia Belanja - Where Quality Finds Its True Home."</p>
@@ -134,7 +323,7 @@ export default function Homepage() {
 
             <div className="flex flex-col items-center">
                 <div className="flex flex-col justify-center items-center lg:items-start p-16 w-fit gap-4">
-                    <h1 className="text-2xl font-semibold w-fit">What Customer Say About Us</h1>
+                    <h1 className="text-2xl font-semibold w-fit text-center">What Customer Say About Us</h1>
                     <div className="text-dbblue flex flex-row flex-wrap gap-4 justify-center">
                         <div className="flex flex-col justify-center text-center items-center w-64 px-4 py-8 rounded-xl bg-white drop-shadow-lg" >
                             <img src={person1} className="w-24 h-24 rounded-full object-cover m-4"></img>
